025ce5b0d16c3afa9781a7b2a7e6c6d93666beda
dev-1603 <53967786+dev-1603@users.noreply.github.com>
2019-12-15 19:11:54 +0530

    gdgdf

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -92,7 +92,7 @@ export default {
       }).onOk(() => {
         this.tasks.splice(index,1)
         this.$q.notify('Task deleted')
-      })
+      }) 
       },
       addtask() {
         this.tasks.push({
